f6e0cfb1036fd7224022390ed9f28630f6d8eca0
Sahiru Gunawardene <sahirug@gmail.com>
2019-03-20 05:49:39 +0530

    return ip from contract

--- dns/dns.go
+++ dns/dns.go
@@ -1,8 +1,10 @@
 package dns
 
 import (
+	"errors"
 	"github.com/sahirug/ethereum-dns/contracts"
 	"github.com/sahirug/ethereum-dns/errorhandler"
+	"github.com/sahirug/ethereum-dns/util"
 	"golang.org/x/net/dns/dnsmessage"
 	"log"
 	"net"
@@ -86,20 +88,76 @@ func (s *DDNService) Query(packet Packet, contract *eddns.Eddns) {
 		if err != nil {
 			log.Printf("Error when trying to resolve %s", fullQ)
 		} else {
-			log.Printf("IP: %s", string(ip[0][:]))
-			//packet.message.Answers = append()
-
+			//log.Printf("IP: %s", string(ip[0][:]))
+			stringIps := util.ConvertByteArrToString(ip)
+			resources, err := convertToDNSResource(q, stringIps)
+			if err != nil {
+				log.Println("error when trying to create resource record")
+				log.Println(err)
+			}
+			packet.message.Answers = append(packet.message.Answers, resources...)
+			sendPacket(s.conn, packet.message, packet.addr)
 		}
 	}
+}
+
+func sendPacket(conn *net.UDPConn, message dnsmessage.Message, addr net.UDPAddr) {
+	packed, err := message.Pack()
+	if err != nil {
+		log.Println("error when trying to pack message")
+		log.Println(err)
+		return
+	}
+	t, err := conn.WriteToUDP(packed, &addr)
+	if err != nil {
+		log.Println("error when trying to write to udp")
+		log.Println(err)
+	}
+	_ = t
+}
+
+func convertToDNSResource(domain string, data []string) ([]dnsmessage.Resource, error) {
+	//log.Fatalf("Ips are %v", data)
+	rName, err := dnsmessage.NewName(domain)
+	none := []dnsmessage.Resource{}
+
+	var resources []dnsmessage.Resource;
+
+	if err != nil {
+		return none, err
+	}
+
+	for i := 0; i < len(data); i++ {
+		var rType dnsmessage.Type
+		var rBody dnsmessage.ResourceBody
+
+		stringIp := data[i]
+
+		rType = dnsmessage.TypeA
+		ip := net.ParseIP(strings.TrimSpace(stringIp))
+		if ip == nil {
+			return none, errors.New("invalid ip")
+		}
+		rBody = &dnsmessage.AResource{A: [4]byte{ip[12], ip[13], ip[14], ip[15]}}
+
+		resource := dnsmessage.Resource{
+			Header: dnsmessage.ResourceHeader{
+				Name:rName,
+				Type:rType,
+				Class:dnsmessage.ClassCSNET,
+				TTL:500,
+			},
+			Body: rBody,
+		}
+		resources = append(resources, resource)
+	}
+	log.Fatalf("resources are %v", resources)
+	return resources, nil
 }
 
 
 func New() DDNService {
 	return DDNService{}
-}
-
-func convertToDNSResource() {
-
 }
 
 func Start(e *eddns.Eddns) *DDNService {
---
+++ util/util.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"log"
+	"regexp"
+)
+
+func ConvertByteArrToString(data [][15]byte) []string {
+	var arr []string;
+	for i := 0; i < len(data); i++ {
+		addr := string(data[i][:])
+		addr = clean(addr)
+		arr = append(arr, addr)
+		//arr = append(arr, "5.5.5.5")
+	}
+	//log.Fatalf("ded")
+	return arr
+}
+
+func clean(s string) string {
+	reg, err := regexp.Compile("[^a-zA-Z0-9.]+")
+	if err != nil {
+		log.Fatal(err)
+	}
+	pString := reg.ReplaceAllString(s, "")
+	return pString
+}
+
